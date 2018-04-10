fd269173fdf28228745428fe1e053880ebc14c15
tiwariabhishek <abhishekt183@gmail.com>
2018-04-10 22:45:35 +0530

    Added print functionality

--- Algorithms/src/algorithms/dynamicprogramming/StockBuySellKTransactions.java
+++ Algorithms/src/algorithms/dynamicprogramming/StockBuySellKTransactions.java
@@ -1,5 +1,8 @@
 package algorithms.dynamicprogramming;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * Date 10/04/2018
  *
@@ -17,18 +20,45 @@ package algorithms.dynamicprogramming;
 
 public class StockBuySellKTransactions {
 
-    public int maxProfitSlowSolution(int[] profits, int k) {
-        int[][] table = new int[k + 1][profits.length];
+    public int maxProfitSlowSolution(int[] prices, int k) {
+        int[][] table = new int[k + 1][prices.length];
         for (int i = 1; i <= k; i++) {
-            for (int j = 1; j < profits.length; j++) {
+            for (int j = 1; j < prices.length; j++) {
                 int maxSoFar = 0;
                 for (int m = 0; m < j; m++) {
-                    maxSoFar = Math.max(maxSoFar, profits[j] - profits[m] + table[i - 1][m]);
+                    maxSoFar = Math.max(maxSoFar, prices[j] - prices[m] + table[i - 1][m]);
                 }
                 table[i][j] = Math.max(table[i][j - 1], maxSoFar);
             }
         }
-        return table[k][profits.length - 1];
+        printActualSolution(table, prices);
+        return table[k][prices.length - 1];
+    }
+
+    private void printActualSolution(int[][] table, int[] prices) {
+        int i = table.length - 1;
+        int j = table[0].length - 1;
+        Deque<Integer> stack = new LinkedList<>();
+        while (true) {
+            if (i == 0 || j == 0) break;
+            else if (table[i][j] == table[i][j - 1]) j--;
+            else {
+                stack.addFirst(prices[j]);
+                int maxDiff = table[i][j] - prices[j];
+                for (int k = j - 1; k >= 0; k--) {
+                    if (table[i - 1][k] - prices[k] == maxDiff) {
+                        i--;
+                        j = k;
+                        stack.addFirst(prices[j]);
+                        break;
+                    }
+                }
+            }
+        }
+        while (!stack.isEmpty()) {
+            System.out.println("Buy at price: " + stack.pollFirst());
+            System.out.println("Sell at price: " + stack.pollFirst());
+        }
     }
 
     public static void main(String args[]) {
